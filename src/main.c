31d29fd108ab88af1a8dcd60ed5c7e09a9b6864c
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2016-06-19 21:17:10 +0800

    Hotfix: Add a resource

--- src/main.c
+++ src/main.c
@@ -2,4 +2,5 @@ int main(void) {
 	printf("Hello, World!");
 	printf("This is to fix a very major issue in the master branch");
 	printf("Do not remove these fixes, or consequences will be imposed on you!");
+	printf("I need a resource!")
 }
