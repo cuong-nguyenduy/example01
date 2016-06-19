f85339600efd769e5eca40473a4076b4d3fbba1a
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2016-06-19 21:35:48 +0800

    Issue 53: Add a new feature

--- src/main.c
+++ src/main.c
@@ -2,4 +2,6 @@ int main(void) {
 	printf("Hello, World!");
 	printf("I am here to solve issue #53");
 	printf("As a matter of fact, this issue #53 requires another resource!");
+	printf("Unknowing about a hotfix, we continue this path by adding a feature");
+	printf("And this is the feature: src/feature.h, src/feature.c");
 }
