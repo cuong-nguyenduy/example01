7c8bcf8357d2c30107e9651dfaa0d7d8d3b98613
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2016-06-19 21:39:25 +0800

    Master: Add two features (feature01 and feature02) into the project

--- src/module01/module01.c
+++ src/module01/module01.c
@@ -1,2 +1,10 @@
 #include module01.h
 
+int module01_feature1(int) {
+	printf("Implementing module01/feature1");
+}
+
+
+int module02_feature2(int) {
+	printf("Implemeting module01/feature2");
+}
--- src/module01/module01.h
+++ src/module01/module01.h
@@ -1,3 +1,4 @@
 # module01 header fine
 #define username "Username"
-
+int module01_feature1(int);
+int module01_feature2(int);
