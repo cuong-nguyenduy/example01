521b231a77adf2b224739c11e3fdaaac6566984f
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2016-06-19 21:33:15 +0800

    Issue 53 - Hot fix: Commit to fix a bug in Issue 53

--- src/main.c
+++ src/main.c
@@ -2,4 +2,6 @@ int main(void) {
 	printf("Hello, World!");
 	printf("I am here to solve issue #53");
 	printf("As a matter of fact, this issue #53 requires another resource!");
+	printf("In issue 53, there is a bug. This is to fix that bug");
+	printf("Issue 53 - hotfix: we have another resource. Too many resources to manage!");
 }
