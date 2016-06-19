f5831de2cb7c500d56d9fbf27a0f2af23ba08a6f
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2016-06-19 21:13:10 +0800

    Master: Fix major issues

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
 int main(void) {
 	printf("Hello, World!");
+	printf("This is to fix a very major issue in the master branch");
+	printf("Do not remove these fixes, or consequences will be imposed on you!");
 }
