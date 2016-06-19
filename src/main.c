b29102858ec980567ce5a2c83010719c327f7f12
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2016-06-19 21:53:23 +0800

    Recommit after merging with Issue53

--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 int main(void) {
 	printf("Hello, World!");
-<<<<<<< HEAD
+// <<<<<<< HEAD
 	printf("This is to fix a very major issue in the master branch");
 	printf("Do not remove these fixes, or consequences will be imposed on you!");
 	printf("I need a resource!")
-=======
+// =======
 	printf("I am here to solve issue #53");
 	printf("As a matter of fact, this issue #53 requires another resource!");
 	printf("Unknowing about a hotfix, we continue this path by adding a feature");
 	printf("And this is the feature: src/feature.h, src/feature.c");
->>>>>>> iss53
+// >>>>>>> iss53
 }
