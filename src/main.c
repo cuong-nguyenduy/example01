9103defa8c62361ebc730af0a126325b59de69f4
Cuong Nguyen <cuong.nguyenduy@gmail.com>
2016-06-19 21:42:53 +0800

    Issue53 - hotfix: modified resource

--- src/main.c
+++ src/main.c
@@ -4,4 +4,5 @@ int main(void) {
 	printf("As a matter of fact, this issue #53 requires another resource!");
 	printf("In issue 53, there is a bug. This is to fix that bug");
 	printf("Issue 53 - hotfix: we have another resource. Too many resources to manage!");
+	printf("Issue 53 - hotfix: Modified the resource.");
 }
